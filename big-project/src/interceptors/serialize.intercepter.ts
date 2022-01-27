570a15f775f2b79e775a2f4d07b28b951edf05e7
DylanMorison <dylan@g.morison.org>
2022-01-26 20:52:12 -0800

    WIP

---
+++ big-project/src/interceptors/serialize.intercepter.ts
@@ -0,0 +1,27 @@
+import { UseInterceptors, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { map, Observable } from 'rxjs';
+
+function Serialize(dto: any) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
+  intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
+    // Run something before a request is handled
+    // by the request handler
+    console.log('***1***: \nIm running before the handler', context);
+
+    return handler.handle().pipe(
+      map((data: any) => {
+        // Run something before the response is sent out
+        console.log('***3***: \nIm running before the response is sent out', data);
+        return plainToInstance(this.dto, data, { excludeExtraneousValues: true });
+      }),
+    );
+  }
+}
+
+export { Serialize, SerializeInterceptor };
--- big-project/src/users/users.controller.ts
+++ big-project/src/users/users.controller.ts
@@ -8,11 +8,16 @@ import {
   Query,
   Delete,
   NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { Serialize } from 'src/interceptors/serialize.intercepter';
+import { UserDto } from './dtos/user.dto';
 
+@Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
@@ -24,6 +29,7 @@ export class UsersController {
 
   @Get('/:id')
   async findUser(@Param('id') id: string) {
+    console.log('***2***: \nHandler is running');
     const user = await this.usersService.findOne(parseInt(id));
     if (!user) {
       throw new NotFoundException('user not found');
